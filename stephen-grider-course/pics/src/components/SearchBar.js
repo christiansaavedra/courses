f5493d7fb2f3d1d794358981f6ce82f58fb2a598
christiansaavedra <saavedra.christian.ok@gmail.com>
2019-10-03 16:12:58 -0300

    pics

---
+++ stephen-grider-course/pics/src/components/SearchBar.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  state = {
+    term: ""
+  };
+
+  onFormSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.term);
+    /* Class based components necesitan usar this.props */
+  };
+
+  render() {
+    return (
+      <div className="ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label>Search: </label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={e => this.setState({ term: e.target.value })}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
